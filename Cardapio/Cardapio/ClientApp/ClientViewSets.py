be7669ca9b4ca31d6c40449ccfd99e4da77ee324
philipehuan <philipehuan@gmail.com>
2019-08-28 16:48:58 -0300

    k

--- Cardapio/Cardapio/ClientApp/ClientViewSets.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.viewsets import  ModelViewSet
-from .ClientSerializers import *
-from .models import Client
-
-
-class ClientViewSet(ModelViewSet):
-    queryset = Client.objects.all()
-    serializer_class = ClientSerializer
-
